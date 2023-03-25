ac93f86552d7b97fa7bbacf08f7c12eb863979cc
smileperez <aa.efimenko@yandex.ru>
2023-03-25 17:00:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HexToRGB from "./components/Hex2rgb";
+
+function App() {
+    return (
+        <>
+            <HexToRGB/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/HexToRGB.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+function HexToRGB() {
+    const [state, setState] = useState({
+        value:''
+    });
+    const element = document.querySelector('.rgb');
+    const handleChange = event => {
+        const val = event.target.value.trim();
+        setState({value: val});
+        if (val.length === 7) {
+            element.value='';
+            let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
+            element.value = result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : 'Ошибка!';
+            if (element.value !== 'Ошибка!') document.body.style.backgroundColor = element.value;
+            else document.body.style.backgroundColor = 'red';
+        }
+    }
+
+    return (
+        <div className={"wrapper"}>
+            <div className={"container"}>
+                <input type={"text"} value={state.value} onChange={handleChange} className={"app-input"}/>
+                <input type={"text"} className={"app-input rgb"}/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default HexToRGB;
